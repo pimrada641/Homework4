a4c99169fe37296c07db0cb1aaf545cf0a78fee9
Pimrada641 <pimrada641@hotmail.com>
2021-04-29 21:29:05 +0700

    added files

---
+++ 1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floor = int.Parse(Console.ReadLine());
+            int c=1;
+
+            while(floor != Math.Abs(floor))
+            {
+                Console.WriteLine("Invalid Pascal's triangle row number.");
+                floor = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i <= floor; i++)
+            {
+                    
+                 for (int j = 0; j <= i; j++)
+                 {
+                        
+                     if (j ==0||j==i)
+                     {
+                        c = 1;
+                     }
+                     else
+                     {
+                        c = c * (i - j + 1) / j;
+                     }
+                     Console.Write(c);
+
+                 }
+                 Console.WriteLine();
+            }
+            Console.ReadLine();
+            
+        }
+
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static bool IsValidSequence(string halfDNASequence)
+        {
+            foreach(char nucleotide in halfDNASequence)
+            {
+                if (!"ATCG".Contains(nucleotide))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static string ReplicateSeqeunce (string halfDNASequence)
+        {
+            string result = "";
+            foreach(char nucleotide in halfDNASequence)
+            {
+                result += "TAGC"["ATCG".IndexOf(nucleotide)];
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string ans;
+            do
+            {
+                string halfDNASequence = Console.ReadLine();
+                IsValidSequence(halfDNASequence);
+                if (IsValidSequence(halfDNASequence) == true)
+                {
+                    Console.WriteLine("Current half DNA sequence : {0}", halfDNASequence);
+                    do
+                    {
+                        Console.Write("Do you want to replicate it ? (Y/N) : ");
+                        ans = Console.ReadLine();
+                        switch (ans)
+                        {
+                            case "Y":
+                                ReplicateSeqeunce(halfDNASequence);
+                                Console.WriteLine("Replicated half DNA sequence : {0}", ReplicateSeqeunce(halfDNASequence));
+                                break;
+                            case "N":
+                                break;
+                            default:
+                                Console.WriteLine("Please input Y or N.");
+                                break;
+
+                        }
+                    } while (ans != "Y" && ans != "N");
+                }
+                else
+                    Console.WriteLine("That half DNA sequence is invalid.");
+                do
+                {
+                    Console.WriteLine("Do you want to process another sequence ? (Y/N) : ");
+                    ans = Console.ReadLine();
+                    if (ans != "Y" && ans != "N")
+                    {
+                        Console.WriteLine("Please input Y or N.");
+                    }
+                } while (ans != "Y" && ans != "N");
+            } while (ans == "Y");
+
+        }
+    }
+}
